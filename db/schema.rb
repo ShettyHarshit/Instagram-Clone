fd996b17648a81493711bc50b05e0ef8f0e6e6ba
Harshit Shetty <shettyharshit5@gmail.com>
2018-03-07 01:42:48 +0530

    Create [Posts Controller and Model]

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180306194458) do
+ActiveRecord::Schema.define(version: 20180306200959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "description"
+    t.integer  "likes"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
