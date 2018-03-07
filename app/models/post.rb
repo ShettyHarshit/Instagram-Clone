fb4cf417203b75c96b0c5899db782fc208536630
Harshit Shetty <shettyharshit5@gmail.com>
2018-03-08 03:36:45 +0530

    Add [Image Upload]

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,4 @@
 class Post < ApplicationRecord
-    def new
-    end
-    
+  has_attached_file :image, styles: { medium: "500x500>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/    
 end
