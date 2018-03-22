a5d86a31b0e7fdf1d09cb14d8b52c04ae6f00265
Harshit Shetty <swaharsh@gmail.com>
2018-03-22 22:58:07 +0530

    Add [Fields added]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180322125931) do
+ActiveRecord::Schema.define(version: 20180322172025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20180322125931) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.string   "url"
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,8 +42,14 @@ ActiveRecord::Schema.define(version: 20180322125931) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "username"
+    t.integer  "followers"
+    t.integer  "following"
+    t.text     "bio"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "posts", "users"
 end
