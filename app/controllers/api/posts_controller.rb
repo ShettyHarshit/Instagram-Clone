4c21ca5fdd2ed36a2306c5cb4ab995a81646cf54
Harshit Shetty <swaharsh@gmail.com>
2018-04-06 00:18:16 +0530

    Add [API for individual posts]

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,8 +1,17 @@
 module Api
     class PostsController < ApplicationController
         def index
-            pokes = Post.order('created_at DESC')
-            render json: {data:pokes}, status: :ok
+            posts = Post.order('created_at DESC')
+            likes = Post.second.weighted_score
+            render json: posts, status: :ok
+        end
+        
+        def show
+            @post = Post.find(params[:id])
+            
+            if stale?(last_modified: @post.updated_at)
+                render json: @post
+            end
         end
     end
 end
