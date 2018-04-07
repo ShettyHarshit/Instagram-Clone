23dcc82702db3bd1cfb276236acd4fbb94c82e77
Harshit Shetty <swaharsh@gmail.com>
2018-04-07 12:17:17 +0530

    Add [Posts CRUD over API]

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -14,6 +14,29 @@ module Api
             end
         end
         
+        def create
+            prod = Post.create!(post_params)
+            
+            if prod.save
+                render json: prod, status: :ok  
+            else
+                render json: prod.errors, status: :ok  
+            end
+        end
+        
+        def update
+            post = Post.find(params[:id])
+            if post.update_attributes(post_params)
+                render json: post, status: :ok  
+            end
+        end
+        
+        def destroy
+            post = Post.find(params[:id])
+            post.destroy
+            render json: post, status: :ok  
+        end
+        
         def like 
             @post = Post.find(params[:post_id])
             @post.liked_by current_user
@@ -27,5 +50,12 @@ module Api
             @post.likes = @post.weighted_score
             render json: @post.likes, status: :ok
         end
+        
+        private
+        
+        def post_params
+            params.permit(:description, :url, :location)
+        end
+        
     end
 end
