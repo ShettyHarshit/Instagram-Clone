d1fee25477e67e8f7c3c8122937e261dfd7f2ac9
Harshit Shetty <swaharsh@gmail.com>
2018-04-09 14:25:15 +0530

    Add [Routing into '/' & '/Users']

--- react/insta/src/App.js
+++ react/insta/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.png';
 import insta from './insta-logo.png';
 import _ from 'lodash';
 import './App.css';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 
 // fetch("http://localhost:3000")
 //   .then(res => res.json())
@@ -16,62 +17,63 @@ class Post extends Component {
     this.state = { loading: true }
     this.x = {};
   }
-
+  
   componentDidMount() {
     this.fetchData();
   }
-
+  
   fetchData = (url ="http://localhost:3001/api/posts") => {
-    this.setState({loading: true})
-    return fetch(url)
-      .then(res => res.json())
-      .then(res => {
-        // this.x = res
-        var t = _.get(res, "data")
-        // var desc = _.map(t, function (o) {
-        //   return o.description
-        // })
-        this.x = t
-        this.setState({loading: false})
-        console.log("Tera Nahi Kata")
-        return t;
-      }).catch(err => {
-        this.setState({ loading: false })
-        console.log("Tera Kat Gaya")
-      })
-  }
+  this.setState({loading: true})
+   return fetch(url)
+     .then(res => res.json())
+     .then(res => {
+       // this.x = res
+       var t = _.get(res, "data");
+       // var desc = _.map(t, function (o) {
+       //   return o.description
+       // })
+       this.x = t
+       this.setState({ loading: false });
+       console.log("Tera Nahi Kata");
+       return t;
+     })
+     .catch(err => {
+       this.setState({ loading: false });
+       console.log("Tera Kat Gaya");
+     });
+}
 
-  render() {
-    var postList = _.map(this.x, (function (post) {
-      return <div className="post-body">
-          <div className="post-user-info">
-            <div className="w10">
-              <img className="post-user-avatar" src="https://instagram.fmaa1-2.fna.fbcdn.net/vp/106ef4c34875eae25833b59bc9754614/5B360E03/t51.2885-19/s150x150/29402975_2106004819686825_2009629057657864192_n.jpg" />
-            </div>
-            <div className="w90">
-              <div className="post-user-name">theharshitshetty </div>
-              <div className="post-user-name">{post.location} </div>
-            </div>
-          </div>
-          <div className="post-wrap">
-            <img src={post.url} />
-            <h1 className="post-likes" key={post.id}>
-              {post.likes} Likes
-            </h1>
-            <p className="post-title">
-              <span className="post-user">theharshitshetty </span>
-              {post.description}
-            </p>
-          </div>
-        </div>;
-    }))
-    return this.state.loading
-      ? <center>Loading</center>
-      : (
-        <div>
-          <div>{postList}</div>
-        {/* {JSON.stringify(this.x)} */}
-        </div>)
+render() {
+  var postList = _.map(this.x, (function (post) {
+    return <div className="post-body">
+    <div className="post-user-info">
+    <div className="w10">
+    <img className="post-user-avatar" src="https://instagram.fmaa1-2.fna.fbcdn.net/vp/106ef4c34875eae25833b59bc9754614/5B360E03/t51.2885-19/s150x150/29402975_2106004819686825_2009629057657864192_n.jpg" />
+    </div>
+    <div className="w90">
+    <div className="post-user-name">theharshitshetty </div>
+    <div className="post-user-name">{post.location} </div>
+    </div>
+    </div>
+    <div className="post-wrap">
+    <img src={post.url} />
+    <h1 className="post-likes" key={post.id}>
+    {post.likes} Likes
+    </h1>
+    <p className="post-title">
+    <span className="post-user">theharshitshetty </span>
+    {post.description}
+    </p>
+    </div>
+    </div>;
+  }))
+  return this.state.loading
+  ? <center>Loading</center>
+  : (
+    <div>
+    <div>{postList}</div>
+    {/* {JSON.stringify(this.x)} */}
+    </div>)
   }
 }
 
@@ -83,23 +85,42 @@ class InstaHeader extends Component {
     </div>;
   }
 }
- 
+
+class Profile extends Component {
+  state = {}
+  render() { 
+    return <div>My Profile
+    <p>
+    x followers y following z posts
+    </p>
+    </div>;
+  }
+}
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <InstaHeader />
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Namma Insta</h1>
-        </header> */}
-        <p className="App-intro">
-        </p>
-        <Post />
+      <div>
+      <InstaHeader />
+      <Router>
+      <div>
+      <Link to="/">Posts</Link>
+      <Link to="/users">Users</Link>
+      <Route path="/users" component={Profile}/>
+      <Route path="/" component={Post}/>
       </div>
-    );
-  }
+      </Router>
+      {/* <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Namma Insta</h1>
+    </header> */}
+    <p className="App-intro">
+    </p>
+    </div>
+  );
+}
 }
 
 export default App;
